204fff27c95e719f005cb2521634d911b6eb3b7b
Caroll Flores <caroll02@hotmail.com>
2024-09-26 20:54:25 -0300

    mejoras jugador, ya anda borrado desde jugador individual, nuevos botones para ir al menu

--- AppDjango/AppDjango/urls.py
+++ AppDjango/AppDjango/urls.py
@@ -16,32 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path, include
-#impotar app con mis vistas
 from loginAdmin import views as loginadmin_views
 from torneo import views as torneo_views
 from jugador import views as jugador_views
-
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     # URLs para la app 
     path('login/', loginadmin_views.admin_login, name='login'),
     path('admin_menu/', loginadmin_views.admin_menu, name='admin_menu'),    
-    path('jugador/<int:jugador_id>/', jugador_views.datos_jugador, name='listado_jugador'),
+    path('jugador/<str:dni>/', jugador_views.datos_jugador, name='listado_jugador'),  # Cambiado a dni
     path('menu/', loginadmin_views.menu, name='menu'),
     path('', torneo_views.index, name='index'),
-    path('admin_carga_jugador/', jugador_views.CrearJugador, name='admin_carga_jugador'),  # Asegúrate de que el nombre coincida aquí
+    path('admin_carga_jugador/', jugador_views.CrearJugador, name='admin_carga_jugador'),
     path('guardar_jugador/', jugador_views.guardar_jugador, name='guardar_jugador'),
     path('listado_jugadores/', jugador_views.listado_jugadores, name='listado_jugadores'),
     path('abm_categoria/', jugador_views.abm_categoria, name='abm_categoria'),
-    path('datos_jugador/<str:dni>/', jugador_views.datos_jugador, name='datos_jugador'),
+    path('datos_jugador/<str:dni>/', jugador_views.datos_jugador, name='datos_jugador'),  # Este está correcto
     path('categorias/', jugador_views.listado_categorias, name='listados_categorias'),
     path('exito_categoria/', jugador_views.exito_categoria, name='exito_categoria'),
     path('categorias/eliminar/<int:id_categoria>/', jugador_views.eliminar_categoria, name='eliminar_categoria'),
-    path('borrado_exitoso/<int:jugador_dni>/', jugador_views.borrado_exitoso, name='borrado_exitoso')
+    path('borrado_exitoso/<str:jugador_dni>/', jugador_views.borrado_exitoso, name='borrado_exitoso'),  # Cambiado a str
+    path('modificar_jugador/<str:dni>/', jugador_views.modificar_jugador, name='modificar_jugador'),
+    path('borrar_jugador/<str:dni>/', jugador_views.borrar_jugador, name='borrar_jugador'),
+    
+
 
 ]
-
