5d2d39a1d7219814696e0914581652133d75c43f
FrancoCorbalan09 <francocorba10@gmail.com>
2025-01-05 18:05:31 -0300

    Paso cambio 05/01/2025

--- AppDjango/torneo/admin.py
+++ AppDjango/torneo/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Torneo,Equipo,TorneoJugador
+from .models import Torneo,Equipo,TorneoJugador,Cancha,Partido
 
 # Register your models here.
 admin.site.register(Torneo)
 admin.site.register(Equipo)
 admin.site.register(TorneoJugador)
+admin.site.register(Cancha)
+admin.site.register(Partido)
