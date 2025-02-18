57425c37c0d3ead2b94cd7100529f5a4cf447e9f
Caroll Flores <caroll02@hotmail.com>
2025-02-17 22:41:50 -0300

    arreglo de estilos, botones atajos en torneos

--- AppDjango/AppDjango/urls.py
+++ AppDjango/AppDjango/urls.py
@@ -38,14 +38,18 @@ urlpatterns = [
     path('admin_carga_jugador/', jugador_views.CrearJugador, name='admin_carga_jugador'),
     path('guardar_jugador/', jugador_views.guardar_jugador, name='guardar_jugador'),
     path('listado_jugadores/', jugador_views.listado_jugadores, name='listado_jugadores'),
-    path('abm_categoria/', jugador_views.abm_categoria, name='abm_categoria'),
+    path('exportar_jugadores_pdf/', jugador_views.exportar_jugadores_pdf, name='exportar_jugadores_pdf'),
     path('datos_jugador/<str:dni>/', jugador_views.datos_jugador, name='datos_jugador'),  # Este está correcto
+
+
+    path('abm_categoria/', jugador_views.abm_categoria, name='abm_categoria'),
     path('jugadores/', jugador_views.listado_categorias, name='listados_categorias'),
     path('exito_categoria/', jugador_views.exito_categoria, name='exito_categoria'),
     path('categorias/eliminar/<int:id_categoria>/', jugador_views.eliminar_categoria, name='eliminar_categoria'),
     path('borrado_exitoso/<str:jugador_dni>/', jugador_views.borrado_exitoso, name='borrado_exitoso'),  # Cambiado a str
     path('modificar_jugador/<str:dni>/', jugador_views.modificar_jugador, name='modificar_jugador'),
     path('borrar_jugador/<str:dni>/', jugador_views.borrar_jugador, name='borrar_jugador'),
+    
     path('abm_torneo/', torneo_views.abm_torneo, name='abm_torneo'),
     path('crear_torneo/', torneo_views.crear_torneo, name='crear_torneo'),
     path('eliminar_torneo/<int:id>/', torneo_views.eliminar_torneo, name='eliminar_torneo'),
